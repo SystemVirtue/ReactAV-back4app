07258d34fc75204a0d23646802d99413aa05b828
SystemVirtue <105450395+SystemVirtue@users.noreply.github.com>
2023-12-11 02:42:58 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 console.log('ReactAV app is running!');
 // Here you will include your actual server code to start your application
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
